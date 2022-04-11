4da15ab5e8e1d7df96f983a44327426c3d1ac4fd
Kristofer <kroldan@ford.com>
2022-04-11 15:25:10 -0400

    Fix function parameter

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const button = document.querySelector("button");
 const output = document.querySelector("#email-output");
 const input = document.querySelector("#email-input");
 
-function displayEmail(email){
+function displayEmail(){
     output.textContent = `${input.value}`
 }
 
